fd2fc85734958eba4c4337eed55b4c1279e48a24
Dave Stevens <hello@davestevens.co.uk>
2022-12-24 11:35:55 +0000

    fix: Sets webpack mode correctly.

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,18 @@
 import App from './App.svelte';
 import './global.css';
 
-const app = new App({
-  target: document.body,
-});
+const app = new App({ target: document.body });
+
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', () => {
+    navigator.serviceWorker.register('./service-worker.js').then((registration) => {
+      // eslint-disable-next-line no-console
+      console.log('SW registered: ', registration);
+    }).catch((registrationError) => {
+      // eslint-disable-next-line no-console
+      console.log('SW registration failed: ', registrationError);
+    });
+  });
+}
 
 export default app;
