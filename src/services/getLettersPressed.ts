dcefa54858c256ef75110803d56aafb2472ed720
Dave Stevens <hello@davestevens.co.uk>
2022-12-25 11:41:27 +0000

    fix: Only allows a-z keys to be entered.

--- src/services/getLettersPressed.ts
+++ src/services/getLettersPressed.ts
@@ -2,7 +2,7 @@ export type GetLettersPressedCallback = (letter: string) => void;
 
 const buildKeyPressListener = (callback: GetLettersPressedCallback) => (event: KeyboardEvent) => {
   const { keyCode } = event;
-  if ((keyCode >= 97 && keyCode <= 122) || (keyCode >= 65 && keyCode <= 90)) {
+  if ((keyCode >= 65 && keyCode <= 90)) {
     callback(event.key.toLocaleUpperCase());
   }
 };
